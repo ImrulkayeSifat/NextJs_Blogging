500add5582bad8dec6772f92885bd659ba95a3aa
ImrulkayeSifat <kayesimrul224@gmail.com>
2022-08-18 03:25:30 +0600

    define type

--- nextjs_frontend/pages/index.tsx
+++ nextjs_frontend/pages/index.tsx
@@ -1,7 +1,16 @@
+import { AxiosResponse } from 'axios'
 import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
+import { fetchCategories } from '../http'
+import { ICategory, ICollectionResponse } from '../types'
 
-const Home: NextPage = () => {
+interface IPropTypes{
+  categories : {
+    items:ICategory[]
+  };
+}
+
+const Home: NextPage<IPropTypes> = ({categories}) => {
   return (
     <div>
       <Head>
@@ -20,9 +29,14 @@ const Home: NextPage = () => {
 }
 
 export const getServerSideProps:GetServerSideProps=async()=>{
+  
+  const {data:categories}:AxiosResponse<ICollectionResponse<ICategory[]>> =await fetchCategories();
+
   return{
     props:{
-      
+      categories:{
+        items:categories.data
+      }
     }
   }
 }
